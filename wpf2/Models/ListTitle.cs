739a6358c20d72bda32bbcf818c97e1b12065f5e
hatekilla <hatekilla@yandex.ru>
2023-12-16 06:40:42 +0300

    xd

---
+++ wpf2/Models/ListTitle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf2.Models;
+
+namespace wpf2.Models
+{
+    class ListTitle : ObservableCollection<Title>
+    {
+        public ListTitle()
+        {
+            DbSet<Title> titles = PageEmployee.DataEntitiesEmployee.Titles;
+            var queryTitle = from title in titles select title;
+            foreach (Title titl in queryTitle)
+            {
+                this.Add(titl);
+            }
+        }
+    }
+}
---
+++ wpf2/Models/Title1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf2.Models
+{
+        public partial class Title1
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; } = null;
+
+            public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+        }
+    }
